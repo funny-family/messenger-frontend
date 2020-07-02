b938b302e9fcc1f2a78a7f4b48217753b385a0e5
funny family <maksvadik228@gmail.com>
2020-07-02 23:57:55 +0700

    added sign in and sing up component fotms

---
+++ src/components/forms/SignIn.vue
@@ -0,0 +1,36 @@
+<template>
+  <form
+    class="sign-in-from"
+    @submit.prevent=""
+  >
+    <div>Sign in</div>
+    <input
+      class="sign-in-from__email"
+      type="text"
+      placeholder="Email"
+    />
+    <input
+      class="sign-in-from__password"
+      type="password"
+      placeholder="Password"
+    />
+    <button type="submit">Sign in</button>
+  </form>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.sign-in-from {
+  min-width: 280px;
+  display: flex;
+  flex-direction: column;
+}
+
+input {
+  position: relative;
+  padding: 8px 12px;
+}
+</style>
---
+++ src/components/forms/SignUp.vue
@@ -0,0 +1,77 @@
+<template>
+  <form class="sign-up-from" @submit.prevent="signUp(userRegistrationData)">
+    <div>Sign up</div>
+    <input
+      class=""
+      type="text"
+      autocomplete="off"
+      placeholder="Username"
+      v-model="userRegistrationData.username"
+    />
+    <input
+      class=""
+      type="text"
+      autocomplete="off"
+      placeholder="Email"
+      v-model="userRegistrationData.email"
+    />
+    <input
+      class=""
+      type="password"
+      autocomplete="off"
+      placeholder="Password"
+      v-model="userRegistrationData.password"
+      >
+    <input
+      class=""
+      type="password"
+      autocomplete="off"
+      placeholder="Confirm password"
+      v-model="userRegistrationData.passwordConfirmation"
+    />
+    <button type="submit">Sign up</button>
+  </form>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+
+export default {
+  data: () => ({
+    userRegistrationData: {
+      username: '',
+      email: '',
+      password: '',
+      passwordConfirmation: ''
+    }
+  }),
+  methods: {
+    ...mapActions(['signUp'])
+    // async test() {
+    //   const url = 'http://localhost:3000/auth/signup';
+    //   const response = await fetch(url, {
+    //     method: 'POST',
+    //     headers: {
+    //       'Content-Type': 'application/x-www-form-urlencoded'
+    //     },
+    //     body: JSON.stringify(this.userRegistrationData)
+    //   });
+    //   const result = await response.json();
+    //   console.log(result);
+    // }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.sign-up-from {
+  min-width: 280px;
+  display: flex;
+  flex-direction: column;
+}
+
+input {
+  position: relative;
+  padding: 8px 12px;
+}
+</style>
