bd32b39cbccd559ddb21080b4bd61d2a016742ec
funny family <maksvadik228@gmail.com>
2020-07-04 00:07:47 +0700

    added camelcase rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,19 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
+    node: true
   },
   extends: [
     'plugin:vue/essential',
-    '@vue/airbnb',
+    '@vue/airbnb'
   ],
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: 'babel-eslint'
   },
   rules: {
+    'camelcase': 'off',
     'comma-dangle': ['error', 'never'],
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-  },
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+  }
 };
