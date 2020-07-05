75c9c76608f4a9d7391902455d6642d98ac12d5b
funny family <maksvadik228@gmail.com>
2020-07-05 20:13:22 +0700

    created second empty test input

---
+++ src/components/test/TestInput2.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="test">
+    adadada
+  </div>
+</template>
+
+<style scoped>
+* { /* variables container */
+  --red: red;
+}
+
+.test {
+  color: var(--red);
+}
+</style>
+
+<script>
+export default {
+  //
+};
+</script>
