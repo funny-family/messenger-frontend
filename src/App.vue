7cdd621cc869d7a41fd3fd158f5ba6252ccc99c3
funny family <maksvadik228@gmail.com>
2020-07-12 01:48:04 +0700

    add test selection color

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,10 @@
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
 
+:root {
+  --selection-color: #f0f0f0;
+}
+
 body {
   margin: 0;
   padding: 0;
@@ -22,4 +26,7 @@ button {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
+
+::-moz-selection { background: var(--selection-color); }
+::selection { background: var(--selection-color); }
 </style>
