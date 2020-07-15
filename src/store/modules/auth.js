e479efce2f6fa8a315052e7c7c24bc74d0fc38ed
funny family <maksvadik228@gmail.com>
2020-07-15 23:28:24 +0700

    completed sign in and sign up actions

---
+++ src/store/modules/auth.js
@@ -0,0 +1,62 @@
+export default {
+  state: {
+    userIsLoggedIn: false
+  },
+  mutations: {
+    SIGN_IN(state) {
+      state.userIsLoggedIn = true;
+    }
+  },
+  actions: {
+    async signUp({ commit }, {
+      username, email, password, password_confirmation
+    }) {
+      try {
+        const userRegistrationData = {
+          username,
+          email,
+          password,
+          password_confirmation
+        };
+        const url = process.env.VUE_APP_SIGN_UP_URL;
+        const response = await fetch(url, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(userRegistrationData)
+        });
+        const result = await response.json();
+        commit('SIGN_IN');
+        console.log('SIGN UP FETCH RESULT!', result);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    async signIn({ commit }, {
+      email, password
+    }) {
+      try {
+        const userLoginData = {
+          email,
+          password
+        };
+        console.log(commit);
+        console.log('userLoginData:', userLoginData);
+        const url = process.env.VUE_APP_SIGN_IN_URL;
+        const response = await fetch(url, {
+          method: 'POST',
+          credentials: 'include', // for cookies
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(userLoginData)
+        });
+        const result = await response.json();
+        console.log('SIGN IN FETCH RESULT!', result);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }
+};
