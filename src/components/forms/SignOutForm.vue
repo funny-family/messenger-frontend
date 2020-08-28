893d8f3570f24c746ac5cc159c1ace5b136192fa
funny family <maksvadik228@gmail.com>
2020-08-28 16:52:50 +0700

    added sign out form component

---
+++ src/components/forms/SignOutForm.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <form
+      class="sign-out-from"
+      @submit.prevent="signOut"
+    >
+      <MultiButton
+        type="submit"
+        :formnovalidate="true"
+      >
+        Sign out
+      </MultiButton>
+    </form>
+  </div>
+</template>
+
+<script>
+import MultiButton from '@/components/buttons/MultiButton.vue';
+
+export default {
+  components: {
+    MultiButton
+  },
+  methods: {
+    signOut() {
+      this.$store.dispatch('signOut');
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
