11707b7575a78c07392f0ebb650b9c8047aa0552
funny family <maksvadik228@gmail.com>
2020-09-07 22:01:16 +0700

    changed api variable version name

--- src/config/index.js
+++ src/config/index.js
@@ -1,12 +1,12 @@
 const domain = 'http://localhost:3000';
-const v1 = '/api/v1';
+const apiVerion1 = '/api/v1';
 
 export default {
   api: {
     url: {
-      signup: `${domain + v1}/auth/signup`,
-      signin: `${domain + v1}/auth/signin`,
-      signout: `${domain + v1}/auth/signout`
+      signup: `${domain + apiVerion1}/auth/signup`,
+      signin: `${domain + apiVerion1}/auth/signin`,
+      signout: `${domain + apiVerion1}/auth/signout`
     }
   }
 };
