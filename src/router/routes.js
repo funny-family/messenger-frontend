f49d4fb3a3bfd522f8c1a649c1403df23dfb9e5d
funny family <maksvadik228@gmail.com>
2020-09-19 23:43:20 +0700

    added Im page

--- src/router/routes.js
+++ src/router/routes.js
@@ -11,27 +11,30 @@ const routes = [
     }
   },
   {
+    path: '/im',
+    component: () => import('@/views/Im'),
+    meta: {
+      title: 'Profile',
+      requiresAuth: true
+    }
+  },
+  {
     path: '/signin',
     component: () => import('@/views/Login'),
     meta: {
-      title: 'Sign in',
-      requiresAuth: false
+      title: 'Sign in'
     }
   },
   {
     path: '/signup',
     component: () => import('@/views/Registration'),
     meta: {
-      title: 'Sign up',
-      requiresAuth: false
+      title: 'Sign up'
     }
   },
   {
     path: '/test',
-    component: () => import('@/views/Test'),
-    meta: {
-      requiresAuth: true
-    }
+    component: () => import('@/views/Test')
   }
 ];
 
