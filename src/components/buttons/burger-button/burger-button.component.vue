f5853c153a47d89e19371440fa20cea2e2332d01
funny family <maksvadik228@gmail.com>
2020-12-22 11:59:58 +0700

    feat: add burger-button component

---
+++ src/components/buttons/burger-button/burger-button.component.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="hamburger-menu-container">
+    <input
+      id="menu__toggler"
+      :isOpened="isOpened"
+      @change="changeMenuState($event.target.checked)"
+      type="checkbox"
+    />
+    <label
+      class="menu__button"
+      :class="{
+        'menu__opened': isOpened
+      }"
+      for="menu__toggler"
+    >
+      <!-- <span></span> -->
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BurgerButton',
+  props: {
+    isOpened: {
+      type: Boolean,
+      required: true,
+      default: false
+    }
+  },
+  methods: {
+    changeMenuState(event) {
+      this.$emit('change', event);
+    }
+  }
+};
+</script>
+
+<style scoped>
+@import url('./burger-button.style.css');
+</style>
---
+++ src/components/buttons/burger-button/index.js
@@ -0,0 +1,4 @@
+import { declareComponent } from '../../../utils';
+import BurgerButton from './burger-button.component.vue';
+
+export default declareComponent(BurgerButton.name, BurgerButton);
