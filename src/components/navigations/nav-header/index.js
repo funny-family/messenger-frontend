c122d7910d8e78d502c3f2541068bbce9aa55ff7
funny family <maksvadik228@gmail.com>
2020-12-23 15:16:39 +0700

    feat: add nav-header component
    The old NavHeader component is replaced by this new one.

---
+++ src/components/navigations/nav-header/index.js
@@ -0,0 +1,4 @@
+import { declareComponent } from '@/utils';
+import NavHeader from './nav-header.component.vue';
+
+export default declareComponent(NavHeader.name, NavHeader);
---
+++ src/components/navigations/nav-header/nav-header.component.vue
@@ -0,0 +1,59 @@
+<template>
+  <header class="nav-header">
+    <h2 class="nav-header__logo">Chatus</h2>
+    <nav
+      class="nav-header__list-container"
+      :class="{
+        'nav-header__list-container__showed': !isMenuOpen
+      }"
+    >
+      <ul class="nav-header__list">
+        <li class="nav-header__item">
+          <router-link
+            to="/signin"
+            v-if="this.$store.state.auth.isUserLoggedIn === false"
+          >
+            Sign in
+          </router-link>
+        </li>
+        <li class="nav-header__item">
+          <router-link
+            to="/signup"
+            v-if="this.$store.state.auth.isUserLoggedIn === false"
+          >
+            Sign up
+          </router-link>
+        </li>
+      </ul>
+    </nav>
+    <BurgerButton
+      class="nav-header__burger-button"
+      :isOpened="isMenuOpen"
+      @change="changeMenuState"
+    />
+  </header>
+</template>
+
+<script>
+import BurgerButton from '@/components/buttons/burger-button';
+
+export default {
+  name: 'NavHeader',
+  components: {
+    BurgerButton
+  },
+  data: () => ({
+    isMenuOpen: false
+  }),
+  methods: {
+    changeMenuState() {
+      this.$data.isMenuOpen = !this.$data.isMenuOpen;
+    }
+  }
+};
+</script>
+
+<style scoped>
+@import url('./nav-header.style.css');
+@import url('./nav-header.media-style.css');
+</style>
