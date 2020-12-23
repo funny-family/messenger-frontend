8d6237dadb716dfd5acffe2aafe4dbe86668664c
funny family <maksvadik228@gmail.com>
2020-12-23 20:44:04 +0700

    feat: add translation logic to all routes

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,22 @@
   <transition
     name="fade-in-down"
     mode="out-in"
+    v-if="isPageLoaded"
   >
     <router-view/>
   </transition>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    isPageLoaded: false
+  }),
+  mounted() {
+    this.$data.isPageLoaded = true;
+  }
+};
+</script>
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
@@ -13,12 +25,3 @@
 @import url('./assets/styles/main.css');
 
 </style>
-
-<script>
-export default {
-  mounted() {
-    // console.log('bla bla bla');
-    // this.$store.dispatch('checkAuth');
-  }
-};
-</script>
