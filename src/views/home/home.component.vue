14202b9417958b10fd041fb17a2c3dc38fc429b4
funny family <maksvadik228@gmail.com>
2020-12-24 23:09:34 +0700

    feat: update home view
    Replace old home view to a new one.

---
+++ src/views/home/home.component.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home-page">
+    <!-- <div class="container"> -->
+      <NavHeader class="nav-header" />
+      <main class="home-page-main">
+        <div class="title-block">
+          <h2>Meet the Chatus!</h2>
+        </div>
+      </main>
+      <NavFooter>disigned by funny family</NavFooter>
+    <!-- </div> -->
+  </div>
+</template>
+
+<script>
+import NavHeader from '@/components/navigations/nav-header';
+import NavFooter from '@/components/navigations/nav-footer';
+
+export default {
+  name: 'Home',
+  components: {
+    NavHeader,
+    NavFooter
+  }
+};
+</script>
+
+<style scoped>
+@import url('./home.style.css');
+</style>
---
+++ src/views/home/index.js
@@ -0,0 +1,4 @@
+import { declareComponent } from '@/utils';
+import Home from './home.component.vue';
+
+export default declareComponent(Home.name, Home);
