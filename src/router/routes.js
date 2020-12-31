801b9b63a833bd6dc4a059ff114366acf6e2540a
funny family <maksvadik228@gmail.com>
2021-01-01 00:06:03 +0700

    refactor: change routes structure
    Each route is now in separete object.

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,28 +1,34 @@
 // https://stackoverflow.com/questions/64974795/vue-js-3-component-doesnt-load-in-vuejs-with-router
 
-const routes = [
-  {
-    path: '/:catchAll(.*)',
-    redirect: {
-      name: 'NotFound'
-    }
-  },
-  {
-    name: 'NotFound',
-    path: '/404',
-    component: () => import('../views/not-found'),
-    meta: {
-      title: 'Page not found!'
-    }
-  },
-  {
-    name: 'Home',
-    path: '/',
-    component: () => import('../views/home'),
-    meta: {
-      title: 'Home'
-    }
+const notFoundPage = {
+  name: 'NotFound',
+  path: '/404',
+  component: () => import('../views/not-found'),
+  meta: {
+    title: 'Page not found!'
   }
+};
+
+const homePage = {
+  name: 'Home',
+  path: '/',
+  component: () => import('../views/home'),
+  meta: {
+    title: 'Home'
+  }
+};
+
+const notExisting = {
+  path: '/:catchAll(.*)',
+  redirect: {
+    name: notFoundPage.name
+  }
+};
+
+const routes = [
+  notExisting,
+  notFoundPage,
+  homePage
 ];
 
 export default routes;
