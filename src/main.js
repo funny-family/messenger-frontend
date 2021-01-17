3fbb14593d7b38e531b879bc41304de8a87ee2cc
funny family <maksvadik228@gmail.com>
2021-01-17 20:02:37 +0700

    refactor: update main.js file
    Сhanged "store" import method.

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import { createApp } from 'vue';
 import { router } from './router';
-import store from './store';
+import { store } from './store';
 import App from './app';
 
 const app = createApp(App);
 
 app.use(router);
+router.app = app;
+// router.isReady().then(() => app.mount('#app'));
+
 app.use(store);
 
 app.mount('#app');
