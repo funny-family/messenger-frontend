7fd0da687e0f352f5162a6dacffa6fb3d67c1828
funny family <maksvadik228@gmail.com>
2021-01-19 22:24:39 +0700

    refactor: update "contacts" component

--- src/views/my/templates/chats/components/contact-list/components/contact/contact.component.vue
+++ src/views/my/templates/chats/components/contact-list/components/contact/contact.component.vue
@@ -1,12 +1,49 @@
 <template>
-  <div class="text">
-    Hello from contact component!
-  </div>
+  <article class="contact">
+    <div class="contact__avatar">{{ avatar }}</div>
+    <div class="contact__name">{{ name }}</div>
+    <div>
+      <span
+        class="contact__name-of-the-message-sender"
+        v-if="nameOfTheMessageSender"
+      >
+        {{ nameOfTheMessageSender }}:
+      </span>
+      <span class="contact__message">{{ message }}</span>
+    </div>
+    <div class="contact__time-of-last-message">{{ timeOfLastMeaasge }}</div>
+    <div class="contact__unread-message">{{ unreadMessages }}</div>
+  </article>
 </template>
 
 <script>
 export default {
-  name: 'Contact'
+  name: 'Contact',
+  props: {
+    avatar: {
+      type: String,
+      required: true
+    },
+    name: {
+      type: String,
+      required: true
+    },
+    nameOfTheMessageSender: {
+      type: String,
+      required: true
+    },
+    message: {
+      type: String,
+      required: true
+    },
+    timeOfLastMeaasge: {
+      type: Number,
+      required: true
+    },
+    unreadMessages: {
+      type: Number
+    }
+  }
 };
 </script>
 
