0b94c9e03d870a86644d4bb9fbb6bb29d1e8adad
funny family <maksvadik228@gmail.com>
2021-01-28 21:41:30 +0700

    feat: add "nav-header" in chats view

---
+++ src/views/my/templates/chats/components/nav-header/nav-header.component.vue
@@ -0,0 +1,16 @@
+<template>
+  <header class="nav-header">
+    <div>This is header</div>
+    <input type="text" />
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'NavHeader'
+};
+</script>
+
+<style scoped>
+@import url('./nav-header.style.css');
+</style>
