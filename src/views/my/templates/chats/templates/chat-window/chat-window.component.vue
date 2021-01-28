7e34d5761fae107aa07badbae668a2277a95411d
funny family <maksvadik228@gmail.com>
2021-01-28 21:42:50 +0700

    feat: add template "chat-window" for "chats" view

---
+++ src/views/my/templates/chats/templates/chat-window/chat-window.component.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="text">
+    Hello from chat-window component!
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ChatWindow'
+};
+</script>
+
+<style scoped>
+@import url('./chat-window.styles.css');
+</style>
