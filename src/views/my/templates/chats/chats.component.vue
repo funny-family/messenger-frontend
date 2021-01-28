8224cb912cf0eddab87cde32d54deb0dfcc2ac5b
funny family <maksvadik228@gmail.com>
2021-01-28 21:32:22 +0700

    refactor: refactor "chats" subview

--- src/views/my/templates/chats/chats.component.vue
+++ src/views/my/templates/chats/chats.component.vue
@@ -1,11 +1,27 @@
 <template>
-  <main class="chats-section">
-    <NavHeader />
-    <ChatList />
-  </main>
+  <div class="chats-section">
+    <div>
+      <NavHeader ref="header" />
+      <ChatList />
+    </div>
+    <main>
+      <router-view v-slot="chats">
+        <transition
+          name="fade-in-down"
+          mode="out-in"
+        >
+          <!-- <keep-alive> -->
+            <component :is="chats.Component" />
+          <!-- </keep-alive> -->
+        </transition>
+      </router-view>
+    </main>
+  </div>
 </template>
 
 <script>
+import { ref, onMounted } from 'vue';
+
 import NavHeader from './components/nav-header';
 import ChatList from './components/chat-list';
 
@@ -14,6 +30,28 @@ export default {
   components: {
     NavHeader,
     ChatList
+  },
+  setup() {
+    const header = ref(null);
+    onMounted(() => {
+      console.log(header.value);
+    });
+
+    // const sticky = header.value.offsetTop;
+
+    // function myFunction() {
+    //   if (window.pageYOffset > sticky) {
+    //     header.value.classList.add('sticky');
+    //   } else {
+    //     header.value.classList.remove('sticky');
+    //   }
+    // }
+
+    // window.onscroll = () => { myFunction(); };
+
+    return {
+      header
+    };
   }
 };
 </script>
