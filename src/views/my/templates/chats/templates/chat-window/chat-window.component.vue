1f15ee3f4d6f7fb81f07b48c6b9c4811fbe12662
funny family <maksvadik228@gmail.com>
2021-02-03 12:39:06 +0700

    refactor: add rerirect link
    Added redirect link to "chats" component for modile verion of app.

--- src/views/my/templates/chats/templates/chat-window/chat-window.component.vue
+++ src/views/my/templates/chats/templates/chat-window/chat-window.component.vue
@@ -1,12 +1,33 @@
 <template>
   <div class="chat-window">
     This current chat id: {{ $route.params.id }}
+    <router-link
+      class="chat-window__redirect-link hidden-redirect-link"
+      :to="{
+        name: chatsRoute
+      }"
+      ref="redirectLink"
+    >
+      ⬅
+    </router-link>
   </div>
 </template>
 
 <script>
+import { ref } from 'vue';
+import { routes } from '@/router';
+
 export default {
-  name: 'ChatWindow'
+  name: 'ChatWindow',
+  setup() {
+    const chatsRoute = routes.my.children[0].name;
+    const redirectLink = ref(null);
+
+    return {
+      chatsRoute,
+      redirectLink
+    };
+  }
 };
 </script>
 
