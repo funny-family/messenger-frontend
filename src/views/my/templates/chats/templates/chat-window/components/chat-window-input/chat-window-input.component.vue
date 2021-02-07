06898da063eac5591b453562e0cb1a5e8b5a26b4
funny family <maksvadik228@gmail.com>
2021-02-07 18:55:54 +0700

    feat: add "chat-window-input" component

---
+++ src/views/my/templates/chats/templates/chat-window/components/chat-window-input/chat-window-input.component.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="chat-window-input">
+    <input type="text" />
+  </div>
+</template>
+
+<script src="./chat-window-input.module.js"></script>
+
+<style scoped src="./chat-window-input.styles.css"></style>
