ba3e8db73725996b8d9d244f539c72f8bb2af0a7
funny family <maksvadik228@gmail.com>
2021-02-10 16:20:32 +0700

    feat: add new component
    Added "chat-list-loading-screen" for "chat-list" component while componend isn't mounted.

---
+++ src/views/my/templates/chats/components/chat-list-loading-screen/chat-list-loading-screen.component.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="clls-container">
+    <LoadingSpinnerRing />
+  </div>
+</template>
+
+<script src="./chat-list-loading-screen.module.js"></script>
+
+<style scoped src="./chat-list-loading-screen.styles.css"></style>
---
+++ src/views/my/templates/chats/components/chat-list-loading-screen/index.js
@@ -0,0 +1,3 @@
+import ChatListLoadingScreen from './chat-list-loading-screen.component.vue';
+
+export default ChatListLoadingScreen;
