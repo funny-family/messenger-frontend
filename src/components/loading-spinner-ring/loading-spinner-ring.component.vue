4b21844f2c6a1379061b5041cf512ad801634d73
funny family <maksvadik228@gmail.com>
2021-02-10 15:02:27 +0700

    feat: add new component
    Added "loading-spinner-ring" component.

---
+++ src/components/loading-spinner-ring/loading-spinner-ring.component.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="lds-ring">
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
+</template>
+
+<script src="./loading-spinner-ring.module.js"></script>
+
+<style scoped src="./loading-spinner-ring.styles.css"></style>
