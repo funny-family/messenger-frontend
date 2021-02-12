e5cd1b3cf0a6a54b8c71ade62b317011e6a02743
funny family <maksvadik228@gmail.com>
2021-02-12 20:49:21 +0700

    chore(stylelint config): add plugin to stylint config
    Updated rulles for stylelint config.

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,14 +1,20 @@
+const sortOrderSmacss = require('stylelint-config-property-sort-order-smacss/generate');
+
 module.exports = {
   extends: [
     'stylelint-config-airbnb',
-    'stylelint-config-rational-order',
+    // 'stylelint-config-rational-order',
     'stylelint-prettier/recommended'
   ],
   plugins: [
     'stylelint-order',
+    'stylelint-config-rational-order/plugin',
     'stylelint-scss'
   ],
   rules: {
-    // additional options
+    'selector-max-id': null,
+    'order/properties-order': [
+      sortOrderSmacss({ emptyLineBefore: 'always' })
+    ]
   }
 };
