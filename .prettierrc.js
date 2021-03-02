2d4b019294c7ce1bf27c2fe8d8a85a99b000e75d
funny family <maksvadik228@gmail.com>
2021-03-02 23:13:55 +0700

    chore: update prettier file

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,7 @@
 // https://habr.com/ru/company/ruvds/blog/428173/?_ga=2.120275210.326577658.1609056196-1162913098.1586269545
 
 module.exports = {
+  htmlWhitespaceSensitivity: 'css',
   jsxBracketSameLine: false,
   bracketSpacing: true,
   trailingComma: 'none',
