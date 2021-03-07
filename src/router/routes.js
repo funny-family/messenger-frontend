ec69201bd7461c0fe508b1aaa8500e4dd75e838c
funny family <maksvadik228@gmail.com>
2021-03-07 23:31:27 +0700

    feat(router): add global routes module

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,117 +1,27 @@
-// https://stackoverflow.com/questions/64974795/vue-js-3-component-doesnt-load-in-vuejs-with-router
-// https://router.vuejs.org/api/#name
+const requiredRoutes = require.context('./routes/', true, /\.(js|ts)$/);
+const routes = {};
 
-export const notFound = (() => {
-  const NotFound = () => import('../views/not-found');
+// prettier-ignore
+function fromCabeToCamelCase(str) {
+  const arr = str.split('-');
+  const capital = arr.map((item, index) => (index ?
+    item.charAt(0).toUpperCase() + item.slice(1).toLowerCase() : item.toLowerCase()));
+  const capitalString = capital.join('');
 
-  return {
-    name: NotFound.name,
-    path: '/404',
-    component: NotFound,
-    meta: {
-      title: 'Page not found!'
-    }
-  };
-})();
+  return capitalString;
+}
 
-export const home = (() => {
-  const Home = () => import('../views/home');
+requiredRoutes.keys().forEach((pathToFile) => {
+  // Check if not entry router file
+  if (pathToFile === './index.js') return;
 
-  return {
-    name: Home.name,
-    path: '/',
-    component: Home,
-    meta: {
-      title: 'Home'
-    }
-  };
-})();
+  /**
+   * example:
+   * fromCabeToCamelCase(./sign-in.route.js) -> './signIn.route.js'.match(/(?<=.)[\w]+/) -> signIn
+   */
+  const routeName = fromCabeToCamelCase(pathToFile).match(/(?<=.)[\w]+/)[0];
 
-export const signin = (() => {
-  const SignIn = () => import('../views/sign-in');
+  routes[routeName] = requiredRoutes(pathToFile).default;
+});
 
-  return {
-    name: SignIn.name,
-    path: '/signin',
-    component: SignIn,
-    meta: {
-      title: 'Sign in'
-    }
-  };
-})();
-
-export const signup = (() => {
-  const SignUp = () => import('../views/sign-up');
-
-  return {
-    name: SignUp.name,
-    path: '/signup',
-    component: SignUp,
-    meta: {
-      title: 'Sign up'
-    }
-  };
-})();
-
-export const my = (() => {
-  const My = () => import('../views/my');
-
-  const chats = (() => {
-    const Chats = () => import('../views/my/templates/chats');
-
-    const chat = (() => {
-      const ChatWindow = () => import('../views/my/templates/chats/templates/chat-window');
-
-      return {
-        name: 'ChatWindow',
-        path: 'chat/:id',
-        component: ChatWindow
-      };
-    })();
-
-    return {
-      name: Chats.name,
-      path: 'chats',
-      component: Chats,
-      meta: {
-        title: 'Chats'
-      },
-      children: [
-        chat
-      ]
-    };
-  })();
-
-  const profile = (() => {
-    const Profile = () => import('../views/my/templates/profile');
-
-    return {
-      name: Profile.name,
-      path: 'profile',
-      component: Profile,
-      meta: {
-        title: 'Profile'
-      }
-    };
-  })();
-
-  return {
-    name: My.name,
-    path: '/my',
-    component: My,
-    redirect: {
-      name: chats.name || profile.name
-    },
-    children: [
-      chats,
-      profile
-    ]
-  };
-})();
-
-export const notExisting = (() => ({
-  path: '/:catchAll(.*)',
-  redirect: {
-    name: notFound.name
-  }
-}))();
+export default routes;
