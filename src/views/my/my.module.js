b9d8bfccad2a585710ed17ea3052f52d0d85348c
funny family <maksvadik228@gmail.com>
2021-04-25 15:54:28 +0700

    refactor: call action from compoennt
    Replaced fetch call for getting chats data to another component.

--- src/views/my/my.module.js
+++ src/views/my/my.module.js
@@ -1,6 +1,3 @@
-import { useRoute } from 'vue-router';
-import { watchEffect } from 'vue';
-
 import { my } from './store';
 
 import NavbarMenu from './components/navbar-menu';
@@ -11,18 +8,7 @@ export default {
     NavbarMenu
   },
   setup() {
-    const route = useRoute();
     const { isChatSelected } = my.state;
-
-    watchEffect((onInvalidate) => {
-      if (route.params.id) {
-        my.actions.setChatSelection();
-      }
-
-      onInvalidate(() => {
-        my.actions.unsetChatSelection();
-      });
-    });
 
     return {
       isChatSelected
