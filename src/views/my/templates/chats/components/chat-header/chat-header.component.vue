616812c91d3996ac39423c9b031bc35d22f8bcb3
funny family <maksvadik228@gmail.com>
2021-04-25 19:06:37 +0700

    refactor: update "chat-header" component

--- src/views/my/templates/chats/components/chat-header/chat-header.component.vue
+++ src/views/my/templates/chats/components/chat-header/chat-header.component.vue
@@ -1,6 +1,18 @@
 <template>
   <header class="chat-header">
-    <input type="text" />
+    <form
+      class="chat-header__form"
+      @submit.prevent
+    >
+      <ClearableInput class="chat-header__input1" />
+      <input
+        v-model.trim="chatName"
+        class="chat-header__input"
+        type="text"
+        @input="onInput($event.target.value)"
+      />
+      <div>{{ chatName }}</div>
+    </form>
   </header>
 </template>
 
--- src/views/my/templates/chats/components/chat-header/chat-header.module.js
+++ src/views/my/templates/chats/components/chat-header/chat-header.module.js
@@ -1,3 +1,44 @@
+import { ref, watch } from 'vue';
+
+import ClearableInput from '../../../../../../components/clearable-input';
+
 export default {
-  name: 'ChatHeader'
+  name: 'ChatHeader',
+  components: {
+    ClearableInput
+  },
+  setup() {
+    function debounce(fn, delay) {
+      let timer = null;
+
+      // eslint-disable-next-line
+      return function () {
+        // eslint-disable-next-line
+        const args = arguments;
+        const context = this;
+
+        clearTimeout(timer);
+
+        timer = setTimeout(() => {
+          fn.apply(context, args);
+        }, delay);
+      };
+    }
+
+    const chatName = ref('');
+    const onInput = debounce((value) => {
+      watch(
+        () => chatName.value,
+        () => {
+          chatName.value = value;
+          // console.log(chatName.value);
+        }
+      );
+    }, 500);
+
+    return {
+      onInput,
+      chatName
+    };
+  }
 };
