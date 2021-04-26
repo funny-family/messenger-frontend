0ace964daa501666f679b10a7f6136f4520f2502
funny family <maksvadik228@gmail.com>
2021-04-26 21:49:37 +0700

    refactor: update "chat-card" component
    Imported "useEmptySlotsChecker" hook to component.

--- src/views/my/templates/chats/components/chat-list/components/chat-card/chat-card.module.js
+++ src/views/my/templates/chats/components/chat-list/components/chat-card/chat-card.module.js
@@ -1,50 +1,6 @@
-import { computed } from 'vue';
-
-/**
- * @param {object} slots
- */
-function useEmptySlotsChecker(slots) {
-  const usedSlots = {};
-
-  function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-  }
-
-  for (const slot of Object.entries({ ...slots })) {
-    /**
-     * example:
-     * capitalizeFirstLetter(senderName) return SenderName
-
-     * [`is${capitalizeFirstLetter(slot[0])}SlotUsed`] ->
-     *  is + SenderName + SlotUsed= isSenderNameSlotUsed
-
-     * computed(() => !!slot[1]) -> convert to slot to Boolean true/false
-     */
-    const nameOfUsedSlot = `is${capitalizeFirstLetter(slot[0])}SlotUsed`;
-    usedSlots[nameOfUsedSlot] = computed(() => !!slot[1]);
-  }
-
-  return usedSlots;
-}
-
-const availableSlots = {};
+import { setup } from './chat.card.setup';
 
 export default {
   name: 'ChatCard',
-  slots: availableSlots,
-  setup(_, { slots }) {
-    const slotChecker = useEmptySlotsChecker(slots);
-
-    /**
-     * export new "slots" object witch contain all slot names of ChatCard
-     */
-    for (const slot of Object.entries({ ...slots })) {
-      const slotName = slot[0];
-      availableSlots[slotName] = slotName;
-    }
-
-    return {
-      slotChecker
-    };
-  }
+  setup
 };
