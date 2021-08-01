568e52f0ef8c2d95b0531d56bde57290f455e1c6
funny family <maksvadik228@gmail.com>
2021-08-01 13:40:42 +0700

    refactor(router): update router

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import routes from './routes';
+/* replace it later with './routes' */
+import routes from './routes/index';
 
 const router = createRouter({
   history: createWebHistory(),
@@ -12,6 +13,6 @@ router.beforeEach((to, from, next) => {
 });
 
 // eslint-disable-next-line
-console.log(router.getRoutes());
+console.table(router.getRoutes());
 
 export { router, routes };
